bbffe456e5d1479d2850e3eba77d6b1fe21c404d
William Tapia <bennyrock20@hotmail.com>
2023-02-04 11:44:47 -0600

    Use template version 1.2.0

--- bill/sri.py
+++ bill/sri.py
@@ -14,8 +14,26 @@ class SRI:
     Class for handling SRI functions
     """
 
-    def __init__(self, emission_date, type, ruc, environment, establishment, point_emission, serie, number, sequence, numeric_code, emission_type,
-                 verification_digit):
+    #         <agenteRetencion>0</agenteRetencion>
+    #         <contribuyenteRimpe>CONTRIBUYENTE RÉGIMEN RIMPE</contribuyenteRimpe>
+
+    def __init__(self,
+                 environment,
+                 type,
+                 billing_name,
+                 company_name,
+                 company_ruc,
+                 establishment,
+                 point_emission,
+                 sequence,
+                 company_address,
+                 company_contribuyente_especial,
+                 company_obligado_contabilidad,
+                 emission_date,
+                 serie, number,
+                 numeric_code,
+                 emission_type,
+                 ):
         """
         @param emission_date: Fecha de emision
         @param type: Tipo de comprobante
@@ -27,17 +45,26 @@ class SRI:
         @param emission_type: Tipo de emision
         @param verification_digit: Digito de verificacion
         """
-        self.emission_date = emission_date
+        # Environment information
         self.type = type
-        self.ruc = ruc
         self.environment = environment == "testing" and "1" or "2"
+        self.emission_type = emission_type
+
+        # Billing information
+        self.company_ruc = company_ruc
+        self.billing_name = billing_name
+        self.company_name = company_name
+        self.company_address = company_address
         self.establishment = establishment
         self.point_emission = point_emission
+        self.company_contribuyente_especial = company_contribuyente_especial
+        self.company_obligado_contabilidad = company_obligado_contabilidad
+
+        # Invoice information
+        self.emission_date = emission_date
         self.serie = serie
         self.number = number
         self.numeric_code = numeric_code
-        self.emission_type = emission_type
-        self.verification_digit = verification_digit
         self.sequence = sequence
 
     def __get_reception_url(self):
@@ -65,9 +92,9 @@ class SRI:
         code_number = str(self.numeric_code).zfill(8)
 
         return (
-                str(self.emission_date.replace("/", "")) # Remove the slashes from the date
+                str(self.emission_date.replace("/", ""))  # Remove the slashes from the date
                 + str(self.type)
-                + str(self.ruc)
+                + str(self.company_ruc)
                 + str(self.environment)
                 + str(self.establishment)
                 + str(self.point_emission)
@@ -75,7 +102,6 @@ class SRI:
                 + str(code_number)
                 + str(self.emission_type)
         )
-
 
     @staticmethod
     def generate_digit_verifier(key):
@@ -125,19 +151,55 @@ class SRI:
 
         access_key = "{}{}".format(key, digit_verifier)
 
-        return loader.get_template("factura_V2.1.0.xml").render({
-            "fechaEmision": self.emission_date,
+        return loader.get_template("factura_V1.1.0.xml").render({
+            # Info Tributaria
             "type": self.type,
-            "ruc": self.ruc,
+            "companyRuc": self.company_ruc,
             "environment": self.environment,
             "serie": self.serie,
+            "razonSocial": self.billing_name,
+            "nombreComercial": self.company_name,
             "number": self.number,
             "numeric_code": self.numeric_code,
             "emission_type": self.emission_type,
-            "verification_digit": self.verification_digit,
             "claveAcceso": access_key,
             "dirMatriz": "Calle 1",
             "secuencial": "000000000",
+
+            "dirEstablecimiento": self.company_address,
+            "contribuyenteEspecial": self.company_contribuyente_especial,
+            "obligadoContabilidad": self.company_obligado_contabilidad,
+
+            # Info de la factura
+
+            "fechaEmision": self.emission_date,
+            # Customer Information
+            # <comercioExterior>{{ comercioExterior  }}</comercioExterior>
+            #         <incoTermFactura>A</incoTermFactura>
+            #         <lugarIncoTerm>lugarIncoTerm0</lugarIncoTerm>
+            #         <paisOrigen>000</paisOrigen>
+            #         <puertoEmbarque>puertoEmbarque0</puertoEmbarque>
+            #         <puertoDestino>puertoDestino0</puertoDestino>
+            #         <paisDestino>000</paisDestino>
+            #         <paisAdquisicion>000</paisAdquisicion>
+            #         <tipoIdentificacionComprador>04</tipoIdentificacionComprador>
+            #         <guiaRemision>000-000-000000000</guiaRemision>
+            "tipoIdentificacionComprador": self.customer_identification_type,
+            "razonSocialComprador": self.customer_name,
+            "identificacionComprador": self.customer_identification,
+            "direccionComprador": self.customer_address,
+
+            # Total Information
+            "totalSinImpuestos": self.total_without_taxes,
+            "totalDescuento": self.total_discount,
+            "totalConImpuestos": self.total_taxes,
+            "propina": self.propina,
+            "importeTotal": self.total,
+            "moneda": self.currency,
+            "pagos": self.payments,
+            "detalles": self.details,
+
+
         })
 
     def sign(self):
@@ -159,20 +221,18 @@ class SRI:
 
         response = client.service.validarComprobante(xml)
 
-
         state = response["estado"]
         messages = response["comprobantes"]["comprobante"][0]["mensajes"]
 
         print(messages)
 
-        return  state == "RECIBIDA"
+        return state == "RECIBIDA"
 
     def get_authorization(self):
         """
         Function to get the authorization of the electronic invoice in the SRI
         """
         pass
-
 
     def get_qr(self):
         """
@@ -185,6 +245,3 @@ class SRI:
         Function to get the xml signed of the electronic invoice
         """
         pass
-
-
-
--- main.py
+++ main.py
@@ -10,16 +10,18 @@ def main():
 
     bill = SRI(
         emission_date="04/02/2023",
-        type="01",
-        ruc="1792146739001",
+        type="1",
         environment="testing",
         serie="001",
+        company_ruc="1792146739001",
+        billing_name="Distribuidora de Suministros Nacional S.A.",
+        company_name="Empresa Importadora y Exportadora de Piezas",
+        company_address="Av. 6 de Diciembre y Av. 10 de Agosto",
         number="000000001",
         numeric_code="12345678",
         establishment="001",
         point_emission="001",
         emission_type="1",
-        verification_digit="1",
         sequence="000000001",
     )
 
